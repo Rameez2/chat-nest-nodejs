1836e4eb55a5c7ce5460117a7e6dc32843133ff4
Rameez <syedrameezshahpesh@gmail.com>
2025-06-28 15:39:07 +0500

    again prt

--- server.js
+++ server.js
@@ -178,6 +178,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
